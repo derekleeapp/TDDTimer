c9ac9ca5671550d9c7626ac62f67ddaef862cac7
Walter Scarborough <walter.scarborough@gmail.com>
2016-10-27 23:03:48 +0900

    Implemented stop() and added valid() to check if repeater is running

--- TDDTimerTests/Components/Repeater/DefaultRepeaterTest.swift
+++ TDDTimerTests/Components/Repeater/DefaultRepeaterTest.swift
@@ -22,6 +22,39 @@ class DefaultRepeaterTest: QuickSpec {
                 expect(closureWasCalled).to(beTrue())
             }
 
+            it("reports valid status when running") {
+                let defaultRepeater = DefaultRepeater()
+
+
+                var isValid = false
+                defaultRepeater.start(0.01) {
+                    isValid = defaultRepeater.valid()
+                }
+
+
+                NSRunLoop.advance()
+
+                expect(isValid).to(beTrue())
+            }
+
+            it("reports invalid status when not running") {
+                let defaultRepeater = DefaultRepeater()
+
+                expect(defaultRepeater.valid()).to(beFalse())
+            }
+
+            it("stops running after calling stop") {
+                let defaultRepeater = DefaultRepeater()
+
+
+                defaultRepeater.start(0.01) {}
+                NSRunLoop.advance()
+                defaultRepeater.stop()
+
+
+                expect(defaultRepeater.valid()).to(beFalse())
+            }
+
         }
     }
 }
